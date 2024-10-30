32df4a4fa33403e2e1b93bb9e0b4eb6611f6f0ca
JimenaPerez22 <jimenaperezmorales12@gmail.com>
2024-10-30 10:36:38 -0400

    updated JAVASCRIPT

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 // YOUR CODE HERE
+const radioInput = document.querySelector("#fav_color");
+const radioOutput = document.querySelector("#radio-output");
+
+//random Number
+const randomNumber = getRandomInt();
+console.log(randomNumber);
+
+const numInput = document.querySelector("#num-input");
+const numOutput = document.querySelector("#num-output");
+
+const displayColor = (e) => {
+    //the color that was selected
+    const color = e.target.value;
+
+    if(color == "blue"){
+        radioOutput.innerHTML = "You picked blue 💙";
+    }else if(color == "green"){
+        radioOutput.innerHTML = "You picked green 💚";
+    }else if(color == "pink"){
+        radioOutput.innerHTML = "You picked pink 💕";
+    }else if(color == "black"){
+        radioOutput.innerHTML = "You picked black 🖤";
+    }
+};
+
+const checkNumber = (e) => {
+    const value = e.target.valueAsNumber;
+    if (value > randomNumber){
+        numOutput.innerHTML = "Not " + value + " ,guess lower!";
+    }else if (value < randomNumber){
+        numOutput.innerHTML = "Not " + value + " ,guess higher!";
+    }else {
+        numOutput.innerHTML = "Hurray! You got it! It's  " + value;
+    }
+}
+
+radioInput.addEventListener('change', displayColor);
+numInput.addEventListener ('change', checkNumber);
+
 
 // getRandomInt() - gets a random number between 1 and 100
 // is_dark(hex) - takes in a hex number (for example #123456) and returns true if it's dark and false if it's light.
